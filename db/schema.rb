e1480793e62547aa3ac3ed2d145ab93fa9af908c
Bruna Scopel <brunascomor@gmail.com>
2020-12-01 15:33:05 +0100

    Implements home page

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_30_144911) do
+ActiveRecord::Schema.define(version: 2020_11_30_152343) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "mansion_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["mansion_id"], name: "index_bookings_on_mansion_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "mansions", force: :cascade do |t|
     t.string "address"
     t.integer "price"
     t.integer "capacity"
     t.string "title"
-    t.string "description"
+    t.text "description"
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_mansions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +50,7 @@ ActiveRecord::Schema.define(version: 2020_11_30_144911) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "mansions"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "mansions", "users"
 end
