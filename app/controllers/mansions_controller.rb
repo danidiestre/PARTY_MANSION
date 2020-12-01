aa1c02659adaa80c2fbd764e5bb28f7611326747
rocionadal <rocioonadal@gmail.com>
2020-12-01 15:03:16 +0100

    create controller for mansions with new and create actions

---
+++ app/controllers/mansions_controller.rb
@@ -0,0 +1,7 @@
+class MansionsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'mansions/new'
+  get 'mansions/create'
   devise_for :users
   root to: 'pages#home'
   resources :mansions, only: [:index, :new, :create]
