61adac0e800992b66f682582c38bb5eba6f1d145
rocionadal <rocioonadal@gmail.com>
2020-12-01 20:24:37 +0100

    update style and html for show page

--- app/controllers/mansions_controller.rb
+++ app/controllers/mansions_controller.rb
@@ -1,30 +1,29 @@
 class MansionsController < ApplicationController
-    def index
-        @mansions = Mansion.all
+  def index
+    @mansions = Mansion.all
+  end
+
+  def show
+    @mansion = Mansion.find(params[:id])
+  end
+
+  def new
+    @mansion = Mansion.new
+  end
+
+  def create
+    @mansion = Mansion.new(mansion_params)
+    # @mansion.user = current_user
+    if @mansion.save!
+      redirect_to mansion_path(@mansion)
+    else
+      render :new
     end
+  end
 
-    def show
-        @mansion = Mansion.find(params[:id])
-    end
+  private
 
-    def new
-        @mansion = Mansion.new
-    end
-
-    def create
-        @mansion = Mansion.new(mansion_params)
-        @mansion.user = current_user
-        if @mansion.save!
-          redirect_to mansion_path(@mansion)
-        else
-          render :new
-        end
-    end
-
-    private
-
-    def mansion_params
-        params.require(:mansion).permit(:address, :price, :capacity, :title, :description, :photo)
-    end
-
+  def mansion_params
+    params.require(:mansion).permit(:address, :price, :capacity, :title, :description, :photo)
+  end
 end
