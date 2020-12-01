fbe07c3636177a6fcb66a225ac0dd0e48c6368c5
rocionadal <rocioonadal@gmail.com>
2020-12-01 19:49:40 +0100

    updates

--- app/controllers/mansions_controller.rb
+++ app/controllers/mansions_controller.rb
@@ -1,17 +1,16 @@
 class MansionsController < ApplicationController
-<<<<<<< HEAD
     def index
         @mansions = Mansion.all
     end
-    
+
     def show
         @mansion = Mansion.find(params[:id])
     end
-    
+
     def new
         @mansion = Mansion.new
     end
-    
+
     def create
         @mansion = Mansion.new(mansion_params)
         @mansion.user = current_user
@@ -21,9 +20,9 @@ class MansionsController < ApplicationController
           render :new
         end
     end
-      
+
     private
-    
+
     def mansion_params
         params.require(:mansion).permit(:address, :price, :capacity, :title, :description, :photo)
     end
