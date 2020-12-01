020482cf136ffe5fbb65070d783649285c8470e7
rocionadal <rocioonadal@gmail.com>
2020-12-01 15:40:01 +0100

    create actions and mansion params

--- app/controllers/mansions_controller.rb
+++ app/controllers/mansions_controller.rb
@@ -1,7 +1,18 @@
 class MansionsController < ApplicationController
   def new
+    @mansion = Mansion.new
   end
 
   def create
+    @mansion = Mansion.new(mansion_params)
+    if @mansion.save!
+      redirect_to mansion_path(@mansion)
+    else
+      render :new
+    end
+  end
+
+  def mansion_params
+    params.require(:car).permit(:address, :price, :capacity, :title, :description, :photo)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'mansions/new'
-  get 'mansions/create'
   devise_for :users
   root to: 'pages#home'
   resources :mansions, only: [:index, :new, :create]
