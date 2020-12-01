eec808bf0c5e5d92b59cf0a05faee8b881ae4893
Jaume Ros <jaumeros77@gmail.com>
2020-12-01 13:06:01 +0100

    Added an image to each mansion instance with open Uri

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,8 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+require "open-uri"
+
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Mansion.destroy_all
@@ -20,26 +21,35 @@ jaume = User.create(email: "jaume@gmail.com", password: "password")
 
 
 
+file_balmes = URI.open('https://unsplash.com/photos/PyFzygP2eNg')
 
-Mansion.create(
+balmes = Mansion.create(
   address: 'Carrer Balmes 382',
   price: 800,
   capacity: 300,
   title: 'Luxurious mansion in Balmes',
   description:'Bring your friends and all your family this mansion fits 300 people and you can do anything you want in it!',
   user: ana
-  )
+)
 
-Mansion.create(
+balmes.photo.attach(io: file_balmes, filename: 'balmes.png', content_type: 'image/png')
+
+
+file_muntaner = URI.open('https://unsplash.com/photos/_hw4aUQ81ic')
+muntaner = Mansion.create(
   address: 'Carrer Muntaner 192',
   price: 10000,
   capacity: 100,
   title: 'Whole building on Muntaner',
   description:'You are renting a whole building so you can literally do whatever you want!',
   user: rocio
-  )
+)
 
-Mansion.create(
+muntaner.photo.attach(io: file_muntaner, filename: 'muntaner.png', content_type: 'image/png')
+
+file_plaza = URI.open('https://unsplash.com/photos/Th2MisvdKr0')
+
+plaza = Mansion.create(
   address: 'Plaza Catalunya 1',
   price: 2000,
   capacity: 500,
@@ -47,24 +57,34 @@ Mansion.create(
   description:'A fantastic location, right next to the ramblas with an amazing view of the city',
   user: jaume
   )
+plaza.photo.attach(io: file_plaza, filename: 'plaza.png', content_type: 'image/png')
 
-Mansion.create(
+file_pearson = URI.open('https://unsplash.com/photos/bbce31pJY-w')
+
+pearson = Mansion.create(
   address: 'Avenida Pearson',
   price: 1200,
   capacity: 200,
   title: '200 person Mansion in Pedralbes',
   description:' Get an amazing view of the city with this incredible mansion all the way up in Pedralbes hidden away. ',
   user: dani
-  )
+)
 
-Mansion.create(
+muntaner.photo.attach(io: file_pearson, filename: 'pearson.png', content_type: 'image/png')
+
+
+file_palamos = URI.open('https://unsplash.com/photos/KRNVePAZwMg')
+
+palamos = Mansion.create(
   address: 'Palamos carrer de laigua',
   price: 1500,
   capacity: 50,
   title: 'Costa Brava Miedeval Castle',
   description:' Bring all your friends and have an amazing experience in this Costa Brava castle!',
   user: bruna
-  )
+)
+
+palamos.photo.attach(io: file_palamos, filename: 'palamos.png', content_type: 'image/png')
 
 
 
