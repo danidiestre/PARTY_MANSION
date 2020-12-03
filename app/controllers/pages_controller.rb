dc3acd28773f31d2621ba6a6d43228cae1a5688e
Ana Bajo <kozicana@yahoo.com>
2020-12-03 11:49:42 +0100

    dashboard route, controller and dashboard/navbar dropdown

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,10 @@ class PagesController < ApplicationController
   def home
     @mansions = Mansion.all.sample(3)
   end
+
+  def dashboard
+    @user_mansions = current_user.mansions
+    @user_bookings = current_user.bookings
+    @user_booking_requests = current_user.requested_bookings
+  end
 end
