82b0e3afdef26c37a2af9d1fef2ab8208c6a3a59
Dani Diestre Tomas <dani.diestre@gmail.com>
2020-12-03 17:15:01 +0100

    calendar with javascript for calculate price

--- app/javascript/plugins/init_flatpickr.js
+++ app/javascript/plugins/init_flatpickr.js
@@ -5,8 +5,22 @@ const initFlatpickr = () => {
     const availabilityElement = document.getElementById('availabilities');
     if (availabilityElement) {
         const availabilities = JSON.parse(availabilityElement.dataset.availabilities);
-        flatpickr(".datepicker", { mode: "range", disable: availabilities, dateFormat: "Y-m-d"});
+        flatpickr(".datepicker", { mode: "range", disable: availabilities, dateFormat: "Y-m-d", onChange: function(selectedDates, dateStr, instance) {
+            const priceElement = document.getElementById('price');
+            const startDate = Date.parse(selectedDates[0]);
+            const endDate = Date.parse(selectedDates[1]);
+
+            const totalDays = (endDate - startDate)/86400000 ; 
+            console.log(totalDays)
+            console.log(priceElement)
+
+    
+            const totalPrice = parseInt(priceElement.dataset.price, 10) * totalDays;
+            priceElement.innerHTML = `<h6 class= price-card > € ${parseInt(priceElement.dataset.price, 10)} x ${totalDays} nights </h6> Total € ${totalPrice}`;
+                 
+        }});
     }
+
 };
 
 export {initFlatpickr}
